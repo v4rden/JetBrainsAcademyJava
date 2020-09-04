358a5cd5ef383337d29305790c1b0692b637f94d
v4rden <denys.variah@gmail.com>
2020-09-04 13:36:58 +0300

    The sign of a number

---
+++ Simple Chatty Bot/Problems/The sign of a number/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sign(int number) {
+        if (number == 0) {
+            return number;
+        }
+
+        if (number > 0) {
+            return +1;
+        }
+
+        return -1;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        System.out.println(sign(n));
+    }
+}
