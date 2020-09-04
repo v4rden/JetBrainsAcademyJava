eff70e9d8ea15c55fabe0b521b148c56ed69d254
v4rden <denys.variah@gmail.com>
2020-09-04 14:25:40 +0300

    Reading integer numbers

---
+++ Simple Chatty Bot/Problems/Reading integer numbers/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+        System.out.println(scan.nextInt());
+    }
+}
