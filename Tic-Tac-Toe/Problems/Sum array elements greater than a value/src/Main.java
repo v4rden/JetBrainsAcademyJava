9f01832d5423d917e1e6534ae4a8eb90aa89bd80
v4rden <denys.variah@gmail.com>
2020-09-04 18:25:13 +0300

    Sum array elements greater than a value

---
+++ Tic-Tac-Toe/Problems/Sum array elements greater than a value/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        var size = scan.nextInt();
+        var arr = new int[size];
+        var sum = 0;
+
+        for (var i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+        var n = scan.nextInt();
+
+        for (var i : arr){
+            if(i > n) {
+                sum += i;
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
