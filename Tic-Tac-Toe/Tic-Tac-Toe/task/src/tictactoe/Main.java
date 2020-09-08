11e96b2ee4fff1c859c965fd3dd8a0f07a75ebd4
v4rden <denys.variah@gmail.com>
2020-09-08 14:08:54 +0300

    Extract strings to variables

--- Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     private static final String upperBorder = "---------";
+    private static final String impossibleMsg = "Impossible";
+    private static final String xWinsMsg = "X wins";
+    private static final String oWinsMsg = "O wins";
+    private static final String notFinishedMsg = "Game not finished";
+    private static final String drawMsg = "Draw";
+    private static final String stringEmpty = "";
+    private static final String rowFormat = "| %c %c %c |";
 
     public static void main(String[] args) {
         var side = 3;
         var scan = new Scanner(System.in);
-        scan.useDelimiter("");
+        scan.useDelimiter(stringEmpty);
         var arr = new char[3][3];
 
         for (int i = 0; i < side; i++) {
@@ -23,19 +30,19 @@ public class Main {
     }
 
     public static String getLine(char p1, char p2, char p3) {
-        return String.format("| %c %c %c |", p1, p2, p3);
+        return String.format(rowFormat, p1, p2, p3);
     }
 
     public static String getWin(char[][] arr, int side) {
         var xWin = false;
         var oWin = false;
 
-        var xCount = getCount(arr,side,'X');
-        var oCount = getCount(arr,side,'O');
+        var xCount = getCount(arr, side, 'X');
+        var oCount = getCount(arr, side, 'O');
         var diff = xCount - oCount;
 
-        if(diff < -1 || diff > 1){
-            return "Impossible";
+        if (diff < -1 || diff > 1) {
+            return impossibleMsg;
         }
 
         for (var i = 0; i < side; i++) {
@@ -75,20 +82,20 @@ public class Main {
         }
 
         if (xWin && oWin) {
-            return "Impossible";
+            return impossibleMsg;
         }
         if (xWin) {
-            return "X wins";
+            return xWinsMsg;
         }
         if (oWin) {
-            return "O wins";
+            return oWinsMsg;
         }
         var hasEmpty = hasEmpty(arr, side);
         if (hasEmpty) {
-            return "Game not finished";
+            return notFinishedMsg;
         }
 
-        return "Draw";
+        return drawMsg;
     }
 
     enum State {
@@ -154,11 +161,11 @@ public class Main {
         return result;
     }
 
-    public static int getCount(char[][] arr, int side, char element){
+    public static int getCount(char[][] arr, int side, char element) {
         var counter = 0;
-        for(int i = 0; i < side; i++){
-            for(int j = 0; j < side; j++){
-                if(arr[i][j] == element){
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                if (arr[i][j] == element) {
                     counter++;
                 }
             }
