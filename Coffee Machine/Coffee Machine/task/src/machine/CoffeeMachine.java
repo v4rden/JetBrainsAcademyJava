d29a7d76ccda873fc69f07a21ac31ab7b2e105b6
v4rden <denys.variah@gmail.com>
2020-09-11 16:58:01 +0300

    Project Coffee Machine 5/6

--- Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        var exit = false;
         var scan = new Scanner(System.in);
-        MachineState.print();
-        printOut("Write action (buy, fill, take):");
-        var input = scan.next();
-        if (input.equals("buy")) {
-            buy(scan);
+        //MachineState.print();
+        while (!exit) {
+            printOut("Write action (buy, fill, take, remaining, exit):");
+            var input = scan.next();
+            if (input.equals("buy")) {
+                buy(scan);
+            }
+            if (input.equals("fill")) {
+                fill(scan);
+            }
+            if (input.equals("take")) {
+                take(scan);
+            }
+            if (input.equals("remaining")) {
+                remaining();
+            }
+            if (input.equals("exit")) {
+                exit = true;
+            }
         }
-        if (input.equals("fill")) {
-            fill(scan);
-        }
-        if (input.equals("take")) {
-            take(scan);
-        }
-        MachineState.print();
+        //MachineState.print();
 
         //System.out.println(Res.HOW_MANY);
         //var cups = scan.nextInt();
@@ -69,26 +78,40 @@ public class CoffeeMachine {
     }
 
     private static void buy(Scanner scan) {
-        printOut("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        var choice = scan.nextInt();
-        if (choice == 1) {
+        printOut("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        var choice = scan.next();
+        if (choice.equals("1")) {
+            if (!MachineState.canMakeEspresso()) {
+                return;
+            }
             MachineState.water -= 250;
             MachineState.beans -= 16;
             MachineState.money += 4;
+            MachineState.dCups--;
         }
-        if (choice == 2) {
+        if (choice.equals("2")) {
+            if (!MachineState.canMakeLatte()) {
+                return;
+            }
             MachineState.water -= 350;
             MachineState.milk -= 75;
             MachineState.beans -= 20;
             MachineState.money += 7;
+            MachineState.dCups--;
         }
-        if (choice == 3) {
+        if (choice.equals("3")) {
+            if (!MachineState.canMakeCappuccino()) {
+                return;
+            }
             MachineState.water -= 200;
             MachineState.milk -= 100;
             MachineState.beans -= 12;
             MachineState.money += 6;
+            MachineState.dCups--;
         }
-        MachineState.dCups--;
+
+        if (choice.equals("back")) {
+        }
     }
 
     private static void fill(Scanner scan) {
@@ -111,6 +134,10 @@ public class CoffeeMachine {
         MachineState.money = 0;
     }
 
+    private static void remaining() {
+        MachineState.print();
+    }
+
     private static class MachineState {
         public static int money = 550;
         public static int water = 400;
@@ -125,6 +152,61 @@ public class CoffeeMachine {
             printOut(String.format("%d of coffee beans", beans));
             printOut(String.format("%d of disposable cups", dCups));
             printOut(String.format("%d of money", money));
+        }
+
+        public static boolean canMakeEspresso() {
+            if (water < 250) {
+                printOut("Sorry, not enough water");
+                return false;
+            }
+            if (beans < 16) {
+                printOut("Sorry, not enough coffee beans");
+                return false;
+            }
+            if (dCups == 0) {
+                printOut("Sorry, not enough disposable cups");
+                return false;
+            }
+            return true;
+        }
+
+        public static boolean canMakeLatte() {
+            if (water < 350) {
+                printOut("Sorry, not enough water");
+                return false;
+            }
+            if (milk < 75) {
+                printOut("Sorry, not enough milk");
+                return false;
+            }
+            if (beans < 20) {
+                printOut("Sorry, not enough coffee beans");
+                return false;
+            }
+            if (dCups == 0) {
+                printOut("Sorry, not enough disposable cups");
+                return false;
+            }
+            return true;
+        }
+        public static boolean canMakeCappuccino() {
+            if (water < 200) {
+                printOut("Sorry, not enough water");
+                return false;
+            }
+            if (milk < 100) {
+                printOut("Sorry, not enough milk");
+                return false;
+            }
+            if (beans < 12) {
+                printOut("Sorry, not enough coffee beans");
+                return false;
+            }
+            if (dCups == 0) {
+                printOut("Sorry, not enough disposable cups");
+                return false;
+            }
+            return true;
         }
     }
 
