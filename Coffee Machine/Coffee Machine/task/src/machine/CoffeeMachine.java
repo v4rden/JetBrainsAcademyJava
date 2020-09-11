59a7a6c81fa9a669934971bb650559425d8736cb
v4rden <denys.variah@gmail.com>
2020-09-11 15:15:27 +0300

    Project Coffee Machine 3/6

--- Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -5,16 +5,42 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         var scan = new Scanner(System.in);
-        System.out.println(Res.HOW_MANY);
-        var cups = scan.nextInt();
-        System.out.println(String.format(
-                "For %d cups of coffee you will need:", cups));
-        System.out.println(String.format(
-                "%d ml of water", cups * 200));
-        System.out.println(String.format(
-                "%d ml of milk", cups * 50));
-        System.out.println(String.format(
-                "%d g of coffee beans", cups * 15));
+        //System.out.println(Res.HOW_MANY);
+        //var cups = scan.nextInt();
+        print("Write how many ml of water the coffee machine has:");
+        var water = scan.nextInt();
+        print("Write how many ml of milk the coffee machine has:");
+        var milk = scan.nextInt();
+        print("Write how many grams of coffee beans the coffee machine has:");
+        var beans = scan.nextInt();
+        print("Write how many cups of coffee you will need:");
+        var targetCups = scan.nextInt();
+
+        var possibleForWater = water / 200;
+        var possibleForMilk = milk / 50;
+        var possibleForBeans = beans / 15;
+
+        var max = possibleForWater > possibleForMilk ? possibleForMilk : possibleForWater;
+        max = max > possibleForBeans ? possibleForBeans : max;
+
+        if (max == targetCups) {
+            print("Yes, I can make that amount of coffee");
+        }
+        if (max < targetCups) {
+            print(String.format("No, I can make only %d cup(s) of coffee", max));
+        }
+        if (max > targetCups) {
+            print(String.format("Yes, I can make that amount of coffee (and even %d more than that)", max - targetCups));
+        }
+
+//        System.out.println(String.format(
+//                "For %d cups of coffee you will need:", cups));
+//        System.out.println(String.format(
+//                "%d ml of water", cups * 200));
+//        System.out.println(String.format(
+//                "%d ml of milk", cups * 50));
+//        System.out.println(String.format(
+//                "%d g of coffee beans", cups * 15));
 
 
 //        System.out.println(Res.START);
@@ -24,6 +50,10 @@ public class CoffeeMachine {
 //        System.out.println(Res.POURING_COFFEE);
 //        System.out.println(Res.POURING_MILK);
 //        System.out.println(Res.READY);
+    }
+
+    private static void print(String str) {
+        System.out.println(str);
     }
 
     public static class Res {
