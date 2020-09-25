e3eef69d099a7d38e64b5edff69c443bcd9f04c3
v4rden <denys.variah@gmail.com>
2020-09-25 17:30:23 +0300

    Project Numeral System Converter 2/6 (WUT)

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -1,15 +1,20 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("10 is " + getBinary(10));
+        var scan = new Scanner(System.in);
+        var input = scan.nextInt();
+        var strange = getLast(input);
+        System.out.println(strange);
+        //System.out.println("10 is " + getBinary(10));
     }
 
-    public static String getBinary(int dec){
+    public static String getBinary(int dec) {
         var cur = dec;
         var sb = new StringBuilder();
-        while(cur > 0){
+        while (cur > 0) {
             var r = cur % 2 == 0 ? 0 : 1;
             sb.append(r);
             cur /= 2;
@@ -17,5 +22,13 @@ public class Main {
         sb.append("b0");
         sb.reverse();
         return sb.toString();
+    }
+
+    public static int getLast(int dec) {
+        if (dec < 8) {
+            return dec;
+        }
+
+        return dec % 8;
     }
 }
