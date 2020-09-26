f8ec630d229f20e529eb8ccbee5facf9d66d4eb3
v4rden <denys.variah@gmail.com>
2020-09-26 17:42:23 +0300

    Piece of alphabet

---
+++ Numeral System Converter/Problems/Piece of alphabet/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        var arr = scan.next().toCharArray();
+        
+        var isOrdered = true;
+        var cur = arr[0];
+
+        for(var i: arr){
+            if(i != cur){
+                isOrdered = false;
+                break;
+            }
+            cur++;
+        }
+
+        System.out.println(isOrdered);
+    }
+}
