e3f7d2c4d1ba3ae82192a3a223e876f1e2322a30
v4rden <denys.variah@gmail.com>
2020-09-27 11:52:17 +0300

    The maximum product of adjacent elements

---
+++ Numeral System Converter/Problems/The maximum product of adjacent elements/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        var size = scan.nextInt();
+        var arr = new int[size];
+        var first = scan.nextInt();
+        var max = 0;
+
+        for (var i = 0; i < size - 1; i++) {
+            var second = scan.nextInt();
+            var product = first * second;
+
+            if (product > max) {
+                max = product;
+            }
+            first = second;
+        }
+
+        System.out.println(max);
+    }
+}
