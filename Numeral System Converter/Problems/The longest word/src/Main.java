6f9c6ad1199cb3baa5e04cc7746bb1f6e1b61655
v4rden <denys.variah@gmail.com>
2020-09-28 20:24:03 +0300

    The longest word

---
+++ Numeral System Converter/Problems/The longest word/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        scan.useDelimiter(" ");
+        String word = "";
+
+        while (scan.hasNext()){
+            var input = scan.next();
+            if(word.length() < input.length()){
+                word = input;
+            }
+        }
+
+        System.out.println(word);
+    }
+}
