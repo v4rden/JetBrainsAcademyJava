68c7cb080d43f78a7b2174540a75854e607891ba
v4rden <denys.variah@gmail.com>
2020-09-29 21:19:51 +0300

    Complex

---
+++ Numeral System Converter/Problems/Complex/src/Main.java
@@ -0,0 +1,22 @@
+class Complex {
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    private double real;
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    private double imaginary;
+}
