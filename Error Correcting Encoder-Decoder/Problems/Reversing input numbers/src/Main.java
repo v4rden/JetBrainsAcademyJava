ca5aa2f19b5b468a9984fbaf79019294e4d8bb73
v4rden <denys.variah@gmail.com>
2020-10-12 20:15:43 +0300

    Reversing input numbers

---
+++ Error Correcting Encoder-Decoder/Problems/Reversing input numbers/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        var a = scan.nextInt();
+        var b = scan.nextInt();
+
+        System.out.printf("%s %s", b, a);
+    }
+}
