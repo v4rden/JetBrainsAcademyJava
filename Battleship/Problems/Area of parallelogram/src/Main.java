a5b3c22205b66c8cd670e7ef01c1269304af85ad
v4rden <denys.variah@gmail.com>
2020-10-23 13:48:15 +0300

    Ared of parallelogram

---
+++ Battleship/Problems/Area of parallelogram/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        countAreaOfParallelogram(10, 8);
+    }
+
+    public static void countAreaOfParallelogram(int b, int h) {
+        System.out.println(b * h);
+    }
+}
